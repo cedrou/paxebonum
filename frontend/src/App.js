64e12c3775e633559b4dacbd43feb8f3c42ef968
Cédric Rousseau <cedrou@gmail.com>
2019-09-05 22:46:33 +0200

    Add BrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,61 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import './App.css';
 import NewSongForm from './NewSongForm.js';
 
-class App extends Component {
-  // state = {users: []}
+function Index() {
+  return <h2>Pax Bonum</h2>;
+}
 
-  // componentDidMount() {
-  //   fetch('/users')
-  //     .then(res => res.json())
-  //     .then(users => this.setState({ users }));
-  // }
+class Users extends Component {
+  state = {users: []}
+
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
 
   render() {
     return (
-      // <div className="App">
-      //   <h1>Nouveau chant</h1>
-
-      //   {this.state.users.map(user =>
-      //     <div key={user.id}>{user.username}</div>
-      //   )}
-      // </div>
       <div className="App">
-        <NewSongForm/>
+        <h2>Repertoire</h2>
+
+        {this.state.users.map(user =>
+          <div key={user.id}>{user.username}</div>
+        )}
+      </div>
+    );
+  }
+
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/users/">Users</Link>
+                </li>
+                <li>
+                  <Link to="/new/">Nouveau chant</Link>
+                </li>
+              </ul>
+            </nav>
+
+            <Route path="/" exact component={Index} />
+            <Route path="/users/" component={Users} />
+            <Route path="/new/"   component={NewSongForm} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
