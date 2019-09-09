4d76ebbe5d723adea751d1a9b4ffb026cbc3d79f
Cedric Rousseau <cedrou@gmail.com>
2019-09-09 18:22:32 +0200

    Added react-bootstrap and convert App component to function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import './App.css';
+import { Navbar, Nav, Container } from "react-bootstrap";
 import EditSong from './EditSong.js';
 import SongLibrary from './SongLibrary.js';
 import ViewSong from './ViewSong.js';
@@ -9,41 +9,33 @@ function Index() {
   return <h2>Pax Bonum</h2>;
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-              <span className="navbar-brand">
-                <img src="/taut.png" width="30" height="30" alt=""/>
-              </span>
-              
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar expand="lg" light bg="light">
+        <Navbar.Brand>
+          <img src="/taut.png" width="30" height="30" alt=""/>
+        </Navbar.Brand>
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <div className="navbar-nav">
-                  <Link className="nav-item nav-link" to="/" >Home</Link>
-                  <Link className="nav-item nav-link" to="/library/" >Répertoire</Link>
-                </div>
-              </div>
-            </nav>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link className="nav-item nav-link" to="/" >Home</Link>
+            <Link className="nav-item nav-link" to="/library/" >Répertoire</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
-            <div className="container">
-              <Route path="/"                 exact component={Index} />
-              <Route path="/library/"         exact component={SongLibrary} />
-              <Route path="/library/view/:id"       component={ViewSong} />
-              <Route path="/library/new"            component={EditSong} />
-              <Route path="/library/edit/:id"       component={EditSong} />
-            </div>
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
+      <Container className="container">
+        <Route path="/"                 exact component={Index} />
+        <Route path="/library/"         exact component={SongLibrary} />
+        <Route path="/library/view/:id"       component={ViewSong} />
+        <Route path="/library/new"            component={EditSong} />
+        <Route path="/library/edit/:id"       component={EditSong} />
+      </Container>
+    </BrowserRouter>
+  );
 }
 
 export default App;
