bae02196b395f73b3bee4f305b969ff131a0fb44
Cédric Rousseau <cedrou@gmail.com>
2019-09-10 15:34:22 +0200

    Use React-Bootstrap in SongLibrary

--- src/SongLibrary.js
+++ src/SongLibrary.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
+import { Container, Row, ButtonToolbar, Button } from "react-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap'
 
 function SongLibrary() {
 
@@ -15,16 +17,16 @@ function SongLibrary() {
       setSongs(json.results);
     }
     fetchData();
-  });
+  }, []);
 
   return (
-    <main>
-      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+    <Container>
+      <Row className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
         <h1 className="h2">Répertoire</h1>
-        <div className="btn-toolbar">
-          <Link role="button" className="btn btn-sm btn-outline-secondary" to="/library/new">Ajouter un chant</Link>
-        </div>
-      </div>
+        <ButtonToolbar className="btn-toolbar">
+          <LinkContainer to="/library/new"><Button size="sm" variant="outline-secondary">Ajouter un chant</Button></LinkContainer>
+        </ButtonToolbar>
+      </Row>
 
       <table className="table table-striped table-sm table-hover">
         <thead>
@@ -42,7 +44,7 @@ function SongLibrary() {
           )}
         </tbody>
       </table>
-    </main>
+    </Container>
   );
 }
 
